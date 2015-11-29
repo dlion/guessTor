e36c714fc1e200e32b8c8f4a1540d54d0fd9cf18
Domenico Luciani <domenicoleoneluciani@gmail.com>
2015-11-29 13:32:43 +0100

    Add check if onion domain is up

--- README.md
+++ README.md
@@ -4,12 +4,13 @@ Bruteforces [.onion] domain.
 ## Usage
 
 ```
->guessTor [-a AMOUNT_OF_DOMAINS] [-t TOP_LEVEL_DOMAIN]
+>guessTor [-a AMOUNT_OF_DOMAINS] [-t TOP_LEVEL_DOMAIN] [-u]
 
 help:
 -h, --help
 -a AMOUNT_OF_DOMAINS, The amount of links the script generate.
 -t TOP_LEVEL_DOMAIN, The top level domain to append to the generated URL, by default is "onion".
+-u, Check if the url is online using ishsup.in's API
 ```
 
 ## Author
--- guesstor.go
+++ guesstor.go
@@ -1,15 +1,24 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"math/rand"
+	"net/http"
 	"time"
 )
+
+type Response struct {
+	Url    string
+	Status string
+}
 
 func main() {
 	amount := flag.Int("a", 1, "amount of links to generate")
 	top := flag.String("t", "onion", "Top domain")
+	up := flag.Bool("u", false, "Check if the onion domain is up")
 	list := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUWXYZ1234567890"
 
 	flag.Parse()
@@ -20,6 +29,28 @@ func main() {
 		for i := range domain {
 			domain[i] = list[rand.Intn(len(list))]
 		}
-		fmt.Printf("http://%s.%s\n", domain, *top)
+		if *up == true {
+			urlUP := fmt.Sprintf("http://ishsup.in/?a=%s.%s&format=json", domain, *top)
+			resp, err := http.Get(urlUP)
+			defer resp.Body.Close()
+			body, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				panic(err)
+			}
+			var data Response
+			err = json.Unmarshal([]byte(body), &data)
+			if err != nil {
+				panic(err)
+			}
+			var status string
+			if data.Status == "200" {
+				status = "up"
+			} else {
+				status = "down"
+			}
+			fmt.Printf("http://%s -- STATUS: %s\n", data.Url, status)
+		} else {
+			fmt.Printf("http://%s.%s\n", domain, *top)
+		}
 	}
 }
